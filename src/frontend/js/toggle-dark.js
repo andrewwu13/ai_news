3f52ee8fb4f5e3a9ae978ac829d09e3b1f09e2ca
andrewwu13 <andrewwuca@gmail.com>
2025-08-13 19:20:31 -0400

    added ham mennu sidebar utils

--- src/frontend/js/toggle-dark.js
+++ src/frontend/js/toggle-dark.js
@@ -1,12 +1,19 @@
 function toggleDarkMode() {
     const body = document.body;
-    const isDark = body.classList.toggle("dark-mode");
-    localStorage.setItem("theme", isDark ? "dark" : "light");
+    const isDark = body.classList.toggle('dark-mode');
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme === "dark") {
-        document.body.classList.add("dark-mode");
+window.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.body.classList.add('dark-mode');
     }
-})
+});
+
+const hamMenu = document.querySelector('.hamburger-menu');
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+    offScreenMenu.classList.toggle('active');
+});
